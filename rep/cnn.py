78a002c1552efd827efcb035a7776c04bd4fbbf2
sanjanaramprasad <sanjanaramprasad1@gmail.com>
2016-10-14 01:51:02 -0400

    Update cnn.py

--- rep/cnn.py
+++ rep/cnn.py
@@ -1,69 +1,107 @@
 import tensorflow as tf
-import dataset
 from tensorflow.python.framework.ops import reset_default_graph
-import utils
+#import utils
 import logging
-
-reset_default_graph()
-
-g = tf.get_default_graph()
-[op.name for op in g.get_operations()]
-
-ds = dataset.read_data_sets('/home/ops/data/',one_hot=True)
-X= tf.placeholder(tf.float32 , [None, 8192])
-Y = tf.placeholder(tf.float32 , [None , 2])
-
-X_tensor = tf.reshape(X,[-1,128,64,1])
-
-filter_size = 5
-n_filters_in = 1
-n_filters_out = 68
-n_output = 2
-W_1 = tf.get_variable( name = 'W' , shape=[filter_size , filter_size ,n_filters_in , n_filters_out],
-                       initializer= tf.random_normal_initializer())
-
-b_1 = tf.get_variable(name = 'b' , shape=[n_filters_out] , initializer = tf.constant_initializer())
-
-h_1 = tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(input= X_tensor , filter = W_1 , strides=[1,2,2,1] , padding = 'SAME'), b_1))
-
-n_filters_in = 68
-n_filters_out = 136
-
-W_2 = tf.get_variable(name = 'W2' , shape = [filter_size, filter_size, n_filters_in, n_filters_out], initializer= tf.random_normal_initializer())
-b_2 = tf.get_variable( name = 'b2' , shape = [n_filters_out] , initializer= tf.constant_initializer())
-h_2 = tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(input = h_1 , filter = W_2 , strides= [1,2,2,1] , padding = 'SAME') , b_2))
-
-h_2_flat = tf.reshape(h_2 , [-1 , 32*16 *n_filters_out])
-
-h_3 , W = utils.linear(h_2_flat , 256 , activation = tf.nn.relu , name = 'fc_1')
-
-Y_pred , W = utils.linear(h_3 , n_output , activation = tf.nn.softmax , name = 'fc_2' )
-
-cross_entropy = -tf.reduce_sum(Y*tf.log(Y_pred + 1e-12))
-optimizer = tf.train.AdamOptimizer().minimize(cross_entropy)
-correct_prediction = tf.equal(tf.argmax(Y_pred, 1 ) , tf.argmax(Y, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction , 'float'))
+import dataset
 
 
-sess = tf.Session()
+sess = tf.InteractiveSession()
+ds = dataset.read_data_sets('/home/ubuntu/',one_hot=True)
+print ds.train.images.shape
+print ds.train.labels.shape
+print ds.test.images.shape
+print ds.test.labels.shape
+x = tf.placeholder(tf.float32, shape=[None, 8192])
+y_ = tf.placeholder(tf.float32, shape=[None, 2])
+
+learning_rate = tf.train.exponential_decay(learning_rate=0.0008,
+                                          global_step= 1,
+                                          decay_steps=ds.train.images.shape[0],
+                                          decay_rate= 0.95,
+                                          staircase=True)
+
+def weight_variable(shape):
+  initial = tf.truncated_normal(shape, stddev=0.1)
+  return tf.Variable(initial)
+
+def bias_variable(shape):
+  initial = tf.constant(0.1, shape=shape)
+  return tf.Variable(initial)
+
+def conv2d(x, W):
+  return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+def max_pool_2x2(x):
+  return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+                        strides=[1, 2, 2, 1], padding='SAME')
+
+
+
+W_conv1 = weight_variable([5, 5, 1, 32])
+b_conv1 = bias_variable([32])
+
+x_image = tf.reshape(x, [-1,128,64,1])
+
+h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+h_pool1 = max_pool_2x2(h_conv1)
+
+
+
+
+#Second layer###
+
+
+W_conv2 = weight_variable([5, 5, 32, 64])
+b_conv2 = bias_variable([64])
+
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+h_pool2 = max_pool_2x2(h_conv2)
+
+W_fc1 = weight_variable([32*16 * 64, 1024])
+b_fc1 = bias_variable([1024])
+
+h_pool2_flat = tf.reshape(h_pool2, [-1, 32*16*64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+
+keep_prob = tf.placeholder(tf.float32)
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+
+
+W_fc2 = weight_variable([1024, 2])
+b_fc2 = bias_variable([2])
+
+y_conv=tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+
+
+
+
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_conv, y_))
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+#cost= tf.nn.l2_loss(y_conv - y_, name="squared_error_cost")
+#train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+#correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
+#accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
 sess.run(tf.initialize_all_variables())
-
-
-
-n_epochs = 10
-for epoch_i in range(n_epochs):
-    for batch_xs, batch_ys in ds.train.next_batch():
-        sess.run(optimizer, feed_dict={
-            X: batch_xs,
-            Y: batch_ys
-        })
-        print(sess.run(accuracy , feed_dict = {X: batch_xs , Y:batch_ys}))
-    print("Epoch number %s"%epoch_i)
-    valid = ds.validation
-    print("Validation",sess.run(accuracy,
-                   feed_dict={
-                       X: valid.images,
-                       Y: valid.labels
-                   }))
-print("Optimzation done")
-print(sess.run(accuracy , feed_dict={X:ds.test.images, Y:ds.test.labels}))
+for i in range(20000):
+	#print i
+	batch_xs , batch_ys = ds.train.next_batch(50)
+	if i%100 == 0:
+		train_accuracy = accuracy.eval(feed_dict={
+        	x:batch_xs, y_: batch_ys, keep_prob: 1.0})
+		#print(sess.run(y_conv , feed_dict={x:batch[0]})) 
+    		print("step %d, training accuracy %g"%(i, train_accuracy))
+  	train_step.run(feed_dict={x: batch_xs, y_: batch_ys, keep_prob: 0.5})
+	
+	if i%200 == 0:
+		validation_accuracy = accuracy.eval(feed_dict={
+                x:ds.validation.images, y_: ds.validation.labels, keep_prob: 1.0})
+		print("step %d, validation accuracy%g"%(i, validation_accuracy))
+        #train_step.run(feed_dict={x: batch_xs, y_: batch_ys, keep_prob: 0.5})
+print("test accuracy %g"%accuracy.eval(feed_dict={
+    x: ds.test.images, y_: ds.test.labels, keep_prob: 1.0}))
+print("Done!!")
